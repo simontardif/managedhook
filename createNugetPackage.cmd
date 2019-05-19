e58bf743322aab0b05a6a491289f712a6ab10a6e
simtiqi <simtiqi@gmail.com>
2019-05-19 15:56:06 -0400

    Create createNugetPackage.cmd

---
+++ createNugetPackage.cmd
@@ -0,0 +1,2 @@
+call "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\MSBuild\15.0\Bin\MSBuild.exe"  /p:Configuration=Release ./ManagedHook/ManagedHook.csproj
+nuget.exe pack ManagedHook.nuspec
